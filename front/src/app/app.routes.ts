a84880c949c886e73e67b8a3a7dd097c648aaa01
D M I <ademirjuniorfertonani@gmail.com>
2025-02-20 15:53:05 -0300

    testes

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
 export const routes: Routes = [
+
+  {path: '', component: ListRenderComponent},
+
 
 ];
