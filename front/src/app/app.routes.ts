46ecc64942f0122d23a04d88775e7babc462d700
D M I <ademirjuniorfertonani@gmail.com>
2025-02-20 19:23:26 -0300

    organise

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent }
+
+];
