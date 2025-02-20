82a99a6f9295e7c4d27ef176bbf24280a2752e68
D M I <ademirjuniorfertonani@gmail.com>
2025-02-20 14:21:55 -0300

    testes

--- front/src/app/components/list-render/list-render.component.ts
+++ front/src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Animal } from '../../Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -9,11 +10,17 @@ import { Component } from '@angular/core';
 })
 export class ListRenderComponent {
 
-  animals =[
-    {name: "Dog", type: "Dog"},
-    {name: "Cat", type: "Cat"},
-    {name: "Bob", type: "Horse"},
-    {name: "Fish", type: "Fish"},
+  animalDetails = '';
+
+  animals : Animal [] =[
+    {name: "Dog", type: "Dog", age: 5},
+    {name: "Cat", type: "Cat", age: 5},
+    {name: "Bob", type: "Horse",age: 5},
+    {name: "Fish", type: "Fish",age: 5},
   ];
+
+  showAge(animal: Animal){
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age}, anos!`;
+  }
 
 }
