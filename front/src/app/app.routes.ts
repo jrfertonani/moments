d492468e7f5c9ccadb9d843c10a75abbc5838637
D M I <ademirjuniorfertonani@gmail.com>
2025-02-20 19:34:54 -0300

    organise

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent }
+  { path: "about", component: AboutComponent },
+  { path: "about/new", component: NewMomentComponent }
 
 ];
---
+++ front/src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  imports: [],
+  templateUrl: './new-moment.component.html',
+  styleUrl: './new-moment.component.css'
+})
+export class NewMomentComponent {
+
+}
